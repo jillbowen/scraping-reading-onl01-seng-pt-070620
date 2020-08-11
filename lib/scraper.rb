64f7d10a1600dbd162b159d189fb1498b7158cd1
Jill Short <jillian.b.short@gmail.com>
2020-08-11 18:42:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
